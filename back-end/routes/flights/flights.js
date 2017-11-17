033e16de4ddb5095d775218e9fae72bd00d02708
chandan29 <chandan.paranjape@gmail.com>
2017-11-17 00:14:28 -0800

    change in flights.js

--- back-end/routes/flights/flights.js
+++ back-end/routes/flights/flights.js
@@ -4,3 +4,20 @@ var flightshandler = express.Router();
 
 
 module.exports = flightshandler;
+
+flightshandler.get('/getFlights',function(req,res){
+  //Input parameters: from city, to city, date, class, adults, seniors, children, infants
+console.log(req.param('from'));
+console.log(req.query.from);
+  res.status(201).json({from:req.query.from,to:req.query.to,date:req.query.date});
+});
+
+flightshandler.get('bookFlight',function(req,res){
+
+});
+
+
+//mongodb atlas link-
+/*
+mongodb://root:root@cluster0-shard-00-00-xper0.mongodb.net:27017,cluster0-shard-00-01-xper0.mongodb.net:27017,cluster0-shard-00-02-xper0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin
+*/
