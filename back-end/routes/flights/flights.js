6d19a66edc151d9078dbeba6ebb9ec7ef6d8271c
chandan29 <chandan.paranjape@gmail.com>
2017-11-17 21:00:31 -0800

    made changes to flights.js 17/11

--- back-end/routes/flights/flights.js
+++ back-end/routes/flights/flights.js
@@ -1,15 +1,29 @@
 var path = require('path');
 var express = require('express');
 var flightshandler = express.Router();
-
-
+var mongo=require('../mongo.js');
+var mongoURL = "mongodb://localhost:27017/kayak";
 module.exports = flightshandler;
 
 flightshandler.get('/getFlights',function(req,res){
   //Input parameters: from city, to city, date, class, adults, seniors, children, infants
+  mongo.connect(mongoURL, function(){
+            console.log('Connected to mongo at: ' + mongoURL);
+            var coll = mongo.collection('carTrip');
+
+            coll.find({"toCity":"San Jose"}).toArray(function(err, user){
+                if (user) {
+                  console.log(user);
+                	res.status(201).json(user);
+                } else {
+
+                	res.status(201).json({wrong:1});
+                }
+            });
+        });
 console.log(req.param('from'));
 console.log(req.query.from);
-  res.status(201).json({from:req.query.from,to:req.query.to,date:req.query.date});
+//  res.status(201).json({from:req.query.from,to:req.query.to,date:req.query.date});
 });
 
 flightshandler.get('bookFlight',function(req,res){
