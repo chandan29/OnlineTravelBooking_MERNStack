af7207734530550dce3cb57c04ffe8b6c2533652
shashank <shashank.singh@sjsu.edu>
2017-11-21 10:44:56 -0800

    Added Booking Successful component with better UI

--- front-end/src/components/BookingSuccessful.js
+++ front-end/src/components/BookingSuccessful.js
@@ -6,6 +6,7 @@ import Carousal1 from './Carousal1';
 import Carousal2 from './Carousal2';
 import BeautifulDown from './BeautifulDown';
 import CarTile from './CarTile';
+import Header from './Header';
 var FontAwesome = require('react-fontawesome');
 
 
@@ -19,16 +20,21 @@ class BookingSuccessful extends Component {
   }
   render() {
     return (
-      <div className="mainBodyBookingSuccessful" style={{backgroundColor: "#E8E8E8"}}>
+      <div style={{backgroundColor: "#E8E8E8", textAlign: "center", minHeight: 800}}>
+      <Header />
+      <div style={{width: "40%", height: "auto",marginTop: 100,marginLeft: "30%"}}>
+        <i className="huge icons">
+        <i aria-hidden="true" style={{color: "green"}}className="sun big loading icon"></i>
+        <i aria-hidden="true" style={{color:"green"}}className="user icon"></i></i>
+        <h1>Booking Successful.</h1>
+        <hr />
+        <h1>Your Booking Details:</h1>
+        <h3>Name: </h3>
+        <h3>Email Id:</h3>
+        <h3>Booking Id:</h3>
+        <h3>Booking Description: </h3>
+    </div>
 
-        <div className="body-success">
-            <div class="icon-success">
-                <FontAwesome name='user' size='3x'/>
-            </div>
-            <div className="text-success">
-              Booking SuccessFul
-            </div>
-        </div>
 
       </div>
 );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,5 +6,6 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
