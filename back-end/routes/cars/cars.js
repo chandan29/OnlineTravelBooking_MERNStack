9e44b35ab5034b0807dba8808d686b7541defbf4
chandan29 <chandan.paranjape@gmail.com>
2017-11-22 00:35:31 -0800

    Logger done,email done

--- back-end/routes/cars/cars.js
+++ back-end/routes/cars/cars.js
@@ -3,11 +3,14 @@ var express = require('express');
 var carshandler = express.Router();
 var mongo=require('../mongo.js');
 var mongoURL = "mongodb://localhost:27017/kayak";
+var fs=require('fs');
+var time = require('time');
 
 module.exports = carshandler;
 
 carshandler.post('/getCars',function(req,res){
   //Input parameters: from city, from date,to date
+  var carCity=req.body.carCity;
   mongo.connect(mongoURL, function(){
             console.log('Connected to mongo at: ' + mongoURL);
             var coll = mongo.collection('car');
@@ -16,72 +19,135 @@ carshandler.post('/getCars',function(req,res){
                 if (user) {
                   console.log(user);
                 	res.status(201).json(user);
+
+
+                  //logger
+
+                  var city=carCity;
+                  var t=time.localtime(Date.now()/1000);
+                  var date=""+t.month+"/"+t.dayOfMonth+"/2017";
+                  var curTime=""+t.hours+":"+t.minutes;
+
+                  fs.appendFile("./public/logging/guestuser.txt", "User queried car listing for the city of "+carCity+" on "+date+" at "+curTime+"\n", function(err) {
+                    if(err) {
+                        res.send({0:0});
+
+                    }
+
+                    console.log("The file was saved!");
+                  });
                 } else {
 
                 	res.status(201).json({wrong:1});
                 }
+
+
             });
         });
-console.log(req.param('from'));
-console.log(req.query.from);
-//  res.status(201).json({from:req.query.from,to:req.query.to,date:req.query.date});
+
 });
 
 
 carshandler.post('/bookCar',function(req,res){
-  /*{
-	"_id" : ObjectId("5a0e91f4cda390e36cece396"),
-	"carId" : 210,
-	"fromCity" : "Chicago",
-	"toCity" : "New York",
-	"fromDate" : "10-29-2017",
-	"toDate" : "11-01-2017",
-	"fareDetails" : 390
-}*/
-
   console.log(req.body);
+  var carCity=req.body.cartile.carCity;
+  var carId=req.body.cartile.carId;
+  var carRate=req.body.cartile.carRate
   mongo.connect(mongoURL, function(){
             console.log('Connected to mongo at: ' + mongoURL);
             var coll = mongo.collection('carTrip');
             console.log(req.body);
             coll.insertOne({carId:req.body.cartile.carId,fromCity:req.body.cartile.carCity,toCity:req.body.cartile.carCity,fromDate:req.body.fromDate,toDate:req.body.toDate,fareDetails:req.body.cartile.carRate},function(err, user){
                 if (user) {
+
                 	res.status(201).json(user);
+
+
+                  var bill = `                                                                                  Receipt
+
+
+
+
+                City:  `+carCity+`
+
+                  Price:`+carRate+`
+                `;
+                  fs.writeFile("./public/carbills/"+carId+".txt", bill, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+
+                    console.log("The file was saved!");
+
+                    var email 	= require("emailjs/email");
+                  var server 	= email.server.connect({
+                     user:    "automailerkayak@gmail.com",
+                     password:"shashankui",
+                     host:    "smtp.gmail.com",
+                     ssl:     true
+                  });
+                  // send the message and get a callback with an error or details of the message that was sent
+                  server.send({
+                     text:    carCity,
+                     from:    "you <automailerkayak@gmail.com>",
+                     to:      "Chandan <chandan.paranjape@gmail.com>, Shashank <shashank.singh9193@gmail.com>,Shripal <shripal555@gmail.com>,Anshit<anshit.sobti@gmail.com>",
+                     subject: carCity,
+                     attachment:
+                    [
+                       {data:"<html>i <i>hope</i> this works!</html>", alternative:true},
+                       {path:"./public/carbills/"+carId+".txt", type:"application/text", name:""+carId+".txt"}
+                    ]
+                  }, function(err, message) { console.log(err || message);});
+                });
+                //write a logger
+                var city=carCity;
+                var t=time.localtime(Date.now()/1000);
+                var date=""+t.month+"/"+t.dayOfMonth+"/2017";
+                var curTime=""+t.hours+":"+t.minutes;
+
+                fs.appendFile("./public/logging/guestuser.txt", "User booked a car from the city of "+city+" on "+date+" at "+curTime+"\n", function(err) {
+                  if(err) {
+                      res.send({0:0});
+
+                  }
+
+                  console.log("The file was saved!");
+                });
+
+
+
                 } else {
 
                 	res.status(201).json({wrong:1});
                 }
-            });
+                            //fs.writeStream('',func)
+
+
+
+                    });
         });
 
+
+
 });
+
+
 
 carshandler.post('/receipt',function(req,res){
-  var email 	= require("emailjs/email");
-var server 	= email.server.connect({
-   user:    "automailerkayak@gmail.com",
-   password:"shashankui",
-   host:    "smtp.gmail.com",
-   ssl:     true
-});
+  var time = require('time');
+  var city="San Jose";
+  var t=time.localtime(Date.now()/1000);
+  var date=""+t.month+"/"+t.dayOfMonth+"/2017";
+  var curTime=""+t.hours+":"+t.minutes;
 
-// send the message and get a callback with an error or details of the message that was sent
-server.send({
-   text:    "With attachment",
-   from:    "you <automailerkayak@gmail.com>",
-   to:      "Chandan <chandan.paranjape@gmail.com>, Shashank <shashank.singh9193@gmail.com>,Shripal <shripal555@gmail.com>,Anshit<anshit.sobti@gmail.com>",
-   subject: "testing emailjs",
-   attachment:
-  [
-     {data:"<html>i <i>hope</i> this works!</html>", alternative:true},
-     {path:"/Users/chandanparanjape/272/README.txt", type:"application/text", name:"README.txt"}
-  ]
-}, function(err, message) { console.log(err || message);
-  if(message){
-    res.json({1:1});
-  }
-  else{
-    res.json({0:0});
-  }
-});
+  fs.appendFile("./public/logging/guestuser.txt", "User queried car listing for the city of "+city+" on "+date+" at "+curTime+"\n", function(err) {
+    if(err) {
+        res.send({0:0});
+
+    }
+
+    console.log("The file was saved!");
+    res.send({1:"User queried car listing for the city of "+city+" on "+date+" at "+curTime+"\n"});
+  });
+
 });
