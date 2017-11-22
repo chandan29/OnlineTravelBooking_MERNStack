269d87de75cefa899cb0cbb6d614af2335a9a42c
chandan29 <chandan.paranjape@gmail.com>
2017-11-22 01:26:58 -0800

    Footer

--- front-end/src/components/NewerHomePage.js
+++ front-end/src/components/NewerHomePage.js
@@ -4,6 +4,7 @@ import * as API from '../api/API';
 import Login from "./Login";
 import Message from "./Message";
 import Welcome from "./Welcome";
+import Footer from "./Footer";
 import Header from './Header';
 import MainBody from './MainBody';
 import MainBodyCar from './MainBodyCar';
@@ -87,7 +88,7 @@ class NewerHomePage extends Component {
                     <div className="opener-image" style={{backgroundColor: "pink",width:"100%", height:500}}>
                           <Header />
                           <MainBody handleCarFetch={this.handleCarFetch}/>
-                          <Message message={this.state.message}/>
+                          <Footer />
                     </div>
                 )}/>
                 <Route exact path="/searchCar" render={() => (
